0d1abd8f3e6f99eaf33256769b6be265bf397823
Amit Kumar <verma.amit0407@gmail.com>
2024-07-20 21:06:23 +0530

    update build and used hashlocationstartegy

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavComponent } from './shared/components/nav/nav.component';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,7 @@ import { NavComponent } from './shared/components/nav/nav.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
